36789bbd1ac47b8f6a5c2dfd87acfb9f4e18d157
João Otávio da Silva <joao.otavio1225@gmail.com>
2025-03-13 16:27:50 -0300

    Working With Value Objects

---
+++ 09_RestWithASPNETUdemy_WorkingWithValueObjects/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,65 @@
+using EvolveDb;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Generic;
+using Serilog;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection
+    , new MySqlServerVersion(new Version(8, 0, 21))));
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
+
+//Versioning API
+builder.Services.AddApiVersioning();
+
+// Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void MigrateDatabase(string connection)
+{
+	try
+	{
+		var evolveConnection = new MySqlConnection(connection);
+		var evolve = new Evolve(evolveConnection, msg => Log.Information(msg))
+		{
+			Locations = new List<string> { "db/migrations", "db/dataset" },
+			IsEraseDisabled = true,
+		};
+		evolve.Migrate();
+	}
+	catch (Exception ex)
+	{
+		Log.Error("Database migration failed", ex);
+		throw;
+	}
+}
+
