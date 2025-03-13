93e2d8f7643223e11d1d43f78f9bdb2a2177375e
João Otávio da Silva <joao.otavio1225@gmail.com>
2025-03-13 11:06:18 -0300

    Splitting Logic

---
+++ 06_RestWithASPNETUdemy_SplittingLogic/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Implementations;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection
+    , new MySqlServerVersion(new Version(8, 0, 21))));
+
+//Versioning API
+builder.Services.AddApiVersioning();
+
+// Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
