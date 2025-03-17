90a33638078c09c508c20e0e1d19273e2f1e386f
João Otávio da Silva <joao.otavio1225@gmail.com>
2025-03-17 12:13:27 -0300

    Working with PATCH Verb

---
+++ 16_RestWithASPNETUdemy_PATHVerb/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindById(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        PersonVO Disable(long id);
+        void Delete(long id);
+    }
+}
