6671b3504f063de4db05ff26c862a68e48bf2d3f
João Otávio da Silva <joao.otavio1225@gmail.com>
2025-04-02 15:34:36 -0300

    Continuous Integration with Github Actions and Azure

---
+++ 20_RestWithASPNETUdemy_DeployToAzure/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IFileBusiness.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string fileName);
+        public Task<FileDetailVO> SaveFileToDisk(UploadFileModel file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(UploadFilesModel file);
+    }
+}
---
+++ 20_RestWithASPNETUdemy_DeployToAzure/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/UploadFilesModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class UploadFilesModel
+    {
+        public List<IFormFile> Files { get; set; }
+    }
+}
